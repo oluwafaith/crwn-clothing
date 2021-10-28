ead7b3cfb38e1b04e8c6ae517b2240d730dd6001
Okpala Faith <okpalafaith1@gmail.com>
2021-10-28 07:23:48 +0100

    completed google signin

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component.jsx';
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.components"
-import {auth} from './firebase/firebase.utils'
+import {auth, createUserProfileDocument} from './firebase/firebase.utils'
 
 class App extends React.Component {
   constructor() {
@@ -18,14 +18,27 @@ class App extends React.Component {
   }
 unsuscribeFromAuth = null
 
-  componentDidMount() {
-  this.unsuscribeFromAuth =  auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
-      console.log(user);
-      
-    })
-  }
+ componentDidMount() {
 
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {              
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
+    });
+ }
+  
+  
   componentWillUnmount() {
     this.unsuscribeFromAuth()
   }
